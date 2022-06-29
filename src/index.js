a0ada480a33daf36a32e458b31b038f6eeb7a864
omarbabou <obabou50@gmail.com>
2022-06-29 23:19:20 +0200

    Install lodash locally

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
+  // eslint-disable-next-line no-undef
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   return element;
