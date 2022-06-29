791d10c48cf39bd26ccd0a7daecae195815674f1
omarbabou <obabou50@gmail.com>
2022-06-30 00:00:47 +0200

    Setup html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,6 +7,11 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Todo List',
+    }),
+  ],
   module: {
     rules: [
       {
