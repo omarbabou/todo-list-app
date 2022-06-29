e50fd0a9cf81d5ad286a115ef5e48f02d58c66fe
omarbabou <obabou50@gmail.com>
2022-06-30 00:10:56 +0200

    Add mode development and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+
   entry: './src/index.js',
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devtool: 'inline-source-map',
+
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Todo List',
+      template: './src/index.html',
     }),
   ],
+
   module: {
     rules: [
       {
