0856d00f6571f3040d54af7098779f70ef5737f7
omarbabou <obabou50@gmail.com>
2022-07-06 21:55:43 +0200

    Add describe removeTask function for testing

--- src/modules/add-remove.test.js
+++ src/modules/add-remove.test.js
@@ -17,3 +17,25 @@ describe('addTask function testing', () => {
     expect(addTask('cooking', [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }, { description: 'cooking', completed: false, index: 2 }]);
   });
 });
+
+describe('removeTask function testing', () => {
+  test('description cooking index 1', () => {
+    expect(removeTask(1, [{ description: 'swimming', completed: false, index: 1 }, { description: 'cooking', completed: false, index: 2 }])).toEqual([{ description: 'cooking', completed: false, index: 1 }]);
+  });
+
+  test('[]', () => {
+    expect(removeTask(1, [{ description: 'swimming', completed: false, index: 1 }])).toEqual([]);
+  });
+
+  test('description swimming index 1', () => {
+    expect(removeTask(0, [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }]);
+  });
+
+  test('description swimming index 1', () => {
+    expect(removeTask(-1, [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }]);
+  });
+
+  test('description swimming index 1', () => {
+    expect(removeTask(3, [{ description: 'swimming', completed: false, index: 1 }])).toEqual([{ description: 'swimming', completed: false, index: 1 }]);
+  });
+});
